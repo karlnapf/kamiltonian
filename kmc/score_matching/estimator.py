1b7211cdf655732efacf0c9f5a0a16af21d9da3e
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-01-27 18:02:37 +0000

    fixed bug

--- kmc/score_matching/estimator.py
+++ kmc/score_matching/estimator.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 def log_pdf_estimate(x, alpha, Z, kernel):
-    k = kernel(x, Z)[0, :]
+    k = kernel(Z, x[np.newaxis,:])[:, 0]
     return alpha.dot(k)
 
 def log_pdf_estimate_grad(x, alpha, Z, kernel_grad):
