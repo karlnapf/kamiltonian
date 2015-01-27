70293c2802a0ab14c57290441b049b186a24c059
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-01-27 18:02:29 +0000

    fixed integrator

--- kmc/hamiltonian/leapfrog.py
+++ kmc/hamiltonian/leapfrog.py
@@ -1,13 +1,10 @@
 import numpy as np
 
+def compute_hamiltonian(Qs, Ps, logq, logp):
+    assert len(Ps) == len(Qs)
+    return np.asarray([-logq(Qs[i]) - logp(Ps[i]) for i in range(len(Qs))])
 
-def leapfrog(q, dlogq, p=None, dlogp=None, step_size=0.3, num_steps=1):
-    assert p is None and dlogp is None or p is not None and dlogp is not None
-    
-    if p is None or dlogp is None:
-        p = np.random.randn(len(q))
-        dlogp = lambda x:-x
-    
+def leapfrog(q, dlogq, p, dlogp, step_size=0.3, num_steps=1):
     # for storing trajectory
     Ps = np.zeros((num_steps + 1, len(p)))
     Qs = np.zeros(Ps.shape)
@@ -19,24 +16,24 @@ def leapfrog(q, dlogq, p=None, dlogp=None, step_size=0.3, num_steps=1):
     Qs[0] = q
     
     # half momentum update
-    p = p - (step_size / 2) * -dlogp(q)
+    p = p - (step_size / 2) * -dlogq(q)
     
     # alternate full variable and momentum updates
     for i in range(num_steps):
-        q = q + step_size * -dlogq(p)
+        q = q + step_size * -dlogp(p)
         Qs[i + 1] = q
 
         # precompute since used for two half-steps
-        dlogp_eval = dlogp(q)
+        dlogq_eval = dlogq(q)
 
-        #  second half momentum update
-        p = p - (step_size / 2) * -dlogp_eval
+        #  first half momentum update
+        p = p - (step_size / 2) * -dlogq_eval
         
         # store p as now fully updated
         Ps[i + 1] = p
-
-        # first half momentum update if not last iteration
+        
+        # second half momentum update
         if i != num_steps - 1:
-            p = p - (step_size / 2) * -dlogp_eval
-    
+            p = p - (step_size / 2) * -dlogq_eval
+
     return Qs, Ps
