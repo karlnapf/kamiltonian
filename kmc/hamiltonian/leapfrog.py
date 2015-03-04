41b59505be09146176fd778275ab0f16c9896f6a
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-04 12:28:26 +0000

    remove run until cycle

--- kmc/hamiltonian/leapfrog.py
+++ kmc/hamiltonian/leapfrog.py
@@ -1,24 +1,27 @@
 import numpy as np
 
-def leapfrog(q, dlogq, p, dlogp, step_size=0.3, num_steps=1, run_until_cycle=False):
+def leapfrog(q, dlogq, p, dlogp, step_size=0.3, num_steps=1, max_steps=None):
+    # check whether to use random number of steps
+    if max_steps is not None:
+        num_steps = np.random.randint(num_steps, max_steps+1)
+    
     # for storing trajectory
-    Ps = []
-    Qs = []
+    Ps = np.zeros((num_steps + 1, len(p)))
+    Qs = np.zeros(Ps.shape)
     
     # create copy of state
     p = np.array(p)
     q = np.array(q)
-    Ps += [p]
-    Qs += [q]
+    Ps[0] = p
+    Qs[0] = q
     
     # half momentum update
     p = p - (step_size / 2) * -dlogq(q)
     
     # alternate full variable and momentum updates
-    i = 0
-    while True:
+    for i in range(num_steps):
         q = q + step_size * -dlogp(p)
-        Qs += [q]
+        Qs[i + 1] = q
 
         # precompute since used for two half-steps
         dlogq_eval = dlogq(q)
@@ -27,20 +30,10 @@ def leapfrog(q, dlogq, p, dlogp, step_size=0.3, num_steps=1, run_until_cycle=Fal
         p = p - (step_size / 2) * -dlogq_eval
         
         # store p as now fully updated
-        Ps += [p]
-        
-        # check whether to stop (epsilon close to start point) and maybe stop
-        if i >= num_steps-1:
-            if run_until_cycle:
-                if np.sqrt(np.linalg.norm(Ps[0] - Ps[-1]) ** 2 + np.linalg.norm(Qs[0] - Qs[-1]) ** 2) < step_size:
-                    break
-            else:
-                break
+        Ps[i + 1] = p
         
         # second half momentum update
-        p = p - (step_size / 2) * -dlogq_eval
-        
-        i += 1
-    Qs = np.asarray(Qs)
-    Ps = np.asarray(Ps)
+        if i != num_steps - 1:
+            p = p - (step_size / 2) * -dlogq_eval
+
     return Qs, Ps
