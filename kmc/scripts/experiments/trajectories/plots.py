ffa383d9432f182644db6efa131fe1a8f395e029
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-04 12:29:38 +0000

    more plotting scripts

--- kmc/scripts/experiments/trajectories/plots.py
+++ kmc/scripts/experiments/trajectories/plots.py
@@ -1,7 +1,8 @@
+from kmc.tools.latex_plot_init import plt
 import numpy as np
-import matplotlib.pyplot as plt
 
-def plot_trajectory_result(fname):
+
+def plot_trajectory_result_mean_median(fname):
     with open(fname, 'r') as f:
             results = np.load(f)
             avg_accept = results["avg_accept"]
@@ -9,37 +10,70 @@ def plot_trajectory_result(fname):
             vols = results["vols"]
             vols_est = results["vols_est"]
             Ds = results["Ds"]
-        
-    avg_accept_mean = np.mean(avg_accept, 0)
-    avg_accept_lower = np.percentile(avg_accept, 5, 0)
-    avg_accept_upper = np.percentile(avg_accept, 95, 0)
     
-    avg_accept_est_mean = np.mean(avg_accept_est, 0)
-    avg_accept_est_lower = np.percentile(avg_accept_est, 5, 0)
-    avg_accept_est_upper = np.percentile(avg_accept_est, 95, 0)
+    plt.plot(Ds, np.median(avg_accept, 0), 'r')
+    plt.plot(Ds, np.percentile(avg_accept, 5, 0), 'r--')
+    plt.plot(Ds, np.percentile(avg_accept, 95, 0), 'r--')
     
-    vol_mean = np.mean(vols, 0)
-    vol_lower = np.percentile(vols, 5, 0)
-    vol_upper = np.percentile(vols, 95, 0)
-    
-    vol_est_mean = np.mean(vols_est, 0)
-    vol_est_lower = np.percentile(vols_est, 5, 0)
-    vol_est_upper = np.percentile(vols_est, 95, 0)
-    
-    plt.plot(Ds, avg_accept_mean, 'r')
-    plt.plot(Ds, avg_accept_lower, 'r--')
-    plt.plot(Ds, avg_accept_upper, 'r--')
-    
-    plt.plot(Ds, avg_accept_est_mean, 'b')
-    plt.plot(Ds, avg_accept_est_lower, 'b--')
-    plt.plot(Ds, avg_accept_est_upper, 'b--')
+    plt.plot(Ds, np.median(avg_accept_est, 0), 'b')
+    plt.plot(Ds, np.percentile(avg_accept_est, 5, 0), 'b--')
+    plt.plot(Ds, np.percentile(avg_accept_est, 95, 0), 'b--')
+    plt.plot(Ds, np.median(avg_accept_est, 0)-np.std(avg_accept_est, 0), 'b--')
+    plt.xscale("log")
+    plt.grid(True)
+    plt.xlim([Ds.min(), Ds.max()])
+    ylim = plt.ylim()
+    plt.ylim([ylim[0], 1.01])
     
     plt.figure()
-    plt.plot(Ds, vol_mean, 'r')
-    plt.plot(Ds, vol_lower, 'r--')
-    plt.plot(Ds, vol_upper, 'r--')
+    plt.plot(Ds, np.median(vols, 0), 'r')
+    plt.plot(Ds, np.percentile(vols, 5, 0), 'r.')
+    plt.plot(Ds, np.percentile(vols, 95, 0), 'r.')
+     
+    plt.plot(Ds, np.median(vols_est, 0), 'b')
+    plt.plot(Ds, np.percentile(vols_est, 5, 0), 'b.')
+    plt.plot(Ds, np.percentile(vols_est, 95, 0), 'b.')
+    plt.show()
+
+def plot_trajectory_result_boxplot(fname):
+    with open(fname, 'r') as f:
+            results = np.load(f)
+            avg_accept = results["avg_accept"]
+            avg_accept_est = results["avg_accept_est"]
+            Ds = results["Ds"]
     
-    plt.plot(Ds, vol_est_mean, 'b')
-    plt.plot(Ds, vol_est_lower, 'b--')
-    plt.plot(Ds, vol_est_upper, 'b--')
+    plt.figure()
+    plt.boxplot(avg_accept_est)
+    ylim = plt.ylim()
+    plt.ylim([ylim[0], 1.01])
+    plt.xticks(np.arange(1,len(Ds)+1), [str(D) for D in Ds])
+    plt.grid(True)
+    plt.figure()
+    plt.boxplot(avg_accept)
+    plt.ylim([ylim[0], 1.01])
+    plt.xticks(np.arange(1,len(Ds)+1), [str(D) for D in Ds])
+    plt.grid(True)
+    plt.show()
+    
+
+def plot_trajectory_result_boxplot_mix(fname):
+    with open(fname, 'r') as f:
+            results = np.load(f)
+            avg_accept = results["avg_accept"]
+            avg_accept_est = results["avg_accept_est"]
+            Ds = results["Ds"]
+    
+    plt.figure()
+    plt.plot(np.log2(Ds)+1, np.mean(avg_accept, 0), 'ro')
+    plt.boxplot(avg_accept_est)
+    ylim = plt.ylim()
+    plt.ylim([ylim[0], 1.01])
+    plt.xticks(np.arange(1,len(Ds)+1), [str(D) for D in Ds])
+    plt.grid(True)
+#     plt.plot(np.log2(Ds)+1, np.percentile(avg_accept, 5, 0), 'r--')
+    plt.legend(["HMC"], loc="lower left")
+    plt.ylabel("Acceptance probability")
+    plt.xlabel(r"$D$")
+    fname_base = fname.split(".")[-2]
+    plt.savefig(fname_base + ".eps", axis_inches='tight')
     plt.show()
