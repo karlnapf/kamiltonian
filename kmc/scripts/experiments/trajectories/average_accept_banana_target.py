e70fa00cf624eb4c41b5f84923ee700d6f81f086
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-04 15:06:40 +0000

    added banana trajectory

---
+++ kmc/scripts/experiments/trajectories/average_accept_banana_target.py
@@ -0,0 +1,27 @@
+import os
+
+from kmc.scripts.experiments.trajectories.independent_jobs_classes.BananaTrajectoryJob import BananaTrajectoryJob
+from kmc.scripts.experiments.trajectories.tools import process
+import numpy as np
+
+
+modulename = __file__.split(os.sep)[-1].split('.')[-2]
+
+if __name__ == "__main__":
+    V = 100.
+    bananicity = 0.03
+    sigma_p = 1.
+    Ds = 2 ** np.arange(11)
+    num_repetitions = 100
+    N = 500
+    lmbda = 1.
+    num_steps = 100
+    max_steps = 1000
+    step_size = .1
+    
+    job_generator = lambda D : BananaTrajectoryJob(N, D, V, bananicity, lmbda,
+                                                   sigma_p, num_steps, step_size,
+                                                   max_steps)
+    
+    process(modulename, job_generator, Ds, num_repetitions, N, lmbda, num_steps,
+            step_size, max_steps)
---
+++ kmc/scripts/experiments/trajectories/independent_jobs_classes/BananaTrajectoryJob.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+
+from kmc.densities.banana import log_banana_pdf, sample_banana
+from kmc.scripts.experiments.trajectories.independent_jobs_classes.TrajectoryJob import TrajectoryJob
+from kmc.tools.Log import logger
+
+
+class BananaTrajectoryJob(TrajectoryJob):
+    def __init__(self,
+                 N, D, V, bananicity, lmbda,
+                 sigma_p,
+                 num_steps, step_size, max_steps=None):
+        TrajectoryJob.__init__(self, N, D, lmbda, sigma_p, num_steps, step_size,
+                               max_steps)
+        
+        self.V = V
+        self.bananicity = bananicity
+
+    @abstractmethod
+    def set_up(self):
+        # target density
+        self.logq = lambda x: log_banana_pdf(x, self.bananicity, self.V)
+        self.dlogq = lambda x: log_banana_pdf(x, self.bananicity, self.V, compute_grad=True)
+        
+        # starting state
+        self.q_sample = lambda: sample_banana(1, self.D, self.bananicity, self.V)[0]
+        
+        logger.info("N=%d, D=%d" % (self.N, self.D))
+        self.Z = sample_banana(self.N, self.D, self.bananicity, self.V)
