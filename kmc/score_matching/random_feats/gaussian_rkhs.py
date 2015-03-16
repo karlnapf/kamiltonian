fbcfd9b713f6384dd57d73ab4bb4f5db4ca84111
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-16 18:27:41 +0000

    some updates to objecive

--- kmc/score_matching/random_feats/gaussian_rkhs.py
+++ kmc/score_matching/random_feats/gaussian_rkhs.py
@@ -1,5 +1,12 @@
+from sklearn.cross_validation import KFold
+
 import numpy as np
 
+def sample_basis(D, m, gamma):
+    omega = gamma*np.random.randn(D, m)
+    u = np.random.uniform(0, 2 * np.pi, m)
+    
+    return omega, u
 
 def feature_map_single(x, omega, u):
     m = 1 if np.isscalar(u) else len(u)
@@ -65,46 +72,6 @@ def feature_map_derivatives(X, omega, u):
 def feature_map_derivatives2(X, omega, u):
     return feature_map_derivatives2_loop(X, omega, u)
 
-def compute_b(X, omega, u):
-    assert len(X.shape) == 2
-    Phi1 = feature_map_derivatives2(X, omega, u)
-    return -np.mean(np.sum(Phi1, 0), 0)
-
-def compute_C(X, omega, u):
-    assert len(X.shape) == 2
-    Phi2 = feature_map_derivatives(X, omega, u)
-    d = X.shape[1]
-    N = X.shape[0]
-    m = Phi2.shape[2]
-    C = np.zeros((m, m))
-    
-    for i in range(N):
-        for ell in range(d):
-            phi2 = Phi2[ell, i]
-            C += np.outer(phi2, phi2)
-    
-    return C / N
-
-def score_matching_sym(X, lmbda, omega, u, b=None, C=None):
-    if b is None:
-        b = compute_b(X, omega, u)
-        
-    if C is None:
-        C = compute_C(X, omega, u)
-        
-    theta = np.linalg.solve(C + lmbda * np.eye(len(C)), b)
-    return theta
-    
-def _objective_sym(X, theta, lmbda, omega, u, b=None, C=None):
-    if b is None:
-        b = compute_b(X, omega, u)
-        
-    if C is None:
-        C = compute_C(X, omega, u)
-    
-    I = np.eye(len(theta))
-    return 0.5 * np.dot(theta, np.dot(C + lmbda * I, theta)) - np.dot(theta, b)
-
 def _objective_sym_completely_manual(X, theta, lmbda, omega, u):
     N = X.shape[0]
     D = X.shape[1]
@@ -143,3 +110,59 @@ def _objective_sym_half_manual(X, theta, lmbda, omega, u):
     J_manual /= N
     J_manual += 0.5 * lmbda * np.dot(theta, theta)
     return J_manual
+
+def compute_b(X, omega, u):
+    assert len(X.shape) == 2
+    Phi1 = feature_map_derivatives2(X, omega, u)
+    return -np.mean(np.sum(Phi1, 0), 0)
+
+def compute_C(X, omega, u):
+    assert len(X.shape) == 2
+    Phi2 = feature_map_derivatives(X, omega, u)
+    d = X.shape[1]
+    N = X.shape[0]
+    m = Phi2.shape[2]
+    C = np.zeros((m, m))
+    
+    for i in range(N):
+        for ell in range(d):
+            phi2 = Phi2[ell, i]
+            C += np.outer(phi2, phi2)
+    
+    return C / N
+
+def score_matching_sym(X, lmbda, omega, u, b=None, C=None):
+    if b is None:
+        b = compute_b(X, omega, u)
+        
+    if C is None:
+        C = compute_C(X, omega, u)
+        
+    theta = np.linalg.solve(C + lmbda * np.eye(len(C)), b)
+    return theta
+    
+def objective(X, theta, lmbda, omega, u, b=None, C=None):
+    if b is None:
+        b = compute_b(X, omega, u)
+        
+    if C is None:
+        C = compute_C(X, omega, u)
+    
+    I = np.eye(len(theta))
+    return 0.5 * np.dot(theta, np.dot(C + lmbda * I, theta)) - np.dot(theta, b)
+
+
+def xvalidate(Z, lmbda, omega, u, n_folds=5, num_repetitions=1):
+    Js = np.zeros((num_repetitions, n_folds))
+    
+    for j in range(num_repetitions):
+        kf = KFold(len(Z), n_folds=n_folds, shuffle=True)
+        for i, (train, test) in enumerate(kf):
+            # train
+            theta = score_matching_sym(Z[train], lmbda, omega, u)
+            
+            # evaluate
+            Js[j, i] = objective(Z[test], theta, lmbda, omega, u)
+    
+    return np.mean(Js, 0)
+
