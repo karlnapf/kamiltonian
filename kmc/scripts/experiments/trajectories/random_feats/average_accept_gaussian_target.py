158c566525dfbe341442f7e626ffa94a58cf6e10
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-17 18:08:57 +0000

    mini changes

--- kmc/scripts/experiments/trajectories/random_feats/average_accept_gaussian_target.py
+++ kmc/scripts/experiments/trajectories/random_feats/average_accept_gaussian_target.py
@@ -10,11 +10,11 @@ modulename = __file__.split(os.sep)[-1].split('.')[-2]
 if __name__ == "__main__":
     sigma_q = 1.
     sigma_p = 1.
-    Ds = 2 ** np.arange(4)
+    Ds = 2 ** np.arange(9)
     num_repetitions = 10
     N = 500
-    lmbda = 0.00001
-    m=N
+    lmbda = 0.0001
+    m = N
     num_steps = 100
     max_steps = 1000
     step_size = .1
