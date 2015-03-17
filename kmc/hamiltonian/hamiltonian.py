4e200c91bdd8a5090cb1bdb2f9caad71908ed536
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-17 18:32:14 +0000

    fix bug in acceptance probability

--- kmc/hamiltonian/hamiltonian.py
+++ kmc/hamiltonian/hamiltonian.py
@@ -9,7 +9,7 @@ def compute_log_accept_pr(q0, p0, Qs, Ps, logq, logp):
     H0 = compute_hamiltonian(q0[np.newaxis, :], p0[np.newaxis, :], logq, logp)
     H = compute_hamiltonian(Qs, Ps, logq, logp)
     
-    return np.minimum(np.zeros(H.shape), H - H0)
+    return np.minimum(np.zeros(H.shape), -H + H0)
 
 def compute_log_det_trajectory(Qs, Ps):
     joint = np.hstack((Qs, Ps))
