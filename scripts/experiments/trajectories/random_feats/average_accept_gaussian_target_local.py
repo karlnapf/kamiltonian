ef907c9e4d1a2472df24288ff4bea52f569aea1a
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-18 15:55:49 +0000

    added a "local" script

---
+++ scripts/experiments/trajectories/random_feats/average_accept_gaussian_target_local.py
@@ -0,0 +1,30 @@
+import os
+
+import numpy as np
+from scripts.experiments.trajectories.independent_jobs_classes.random_feats.GaussianTrajectoryJob import GaussianTrajectoryJob
+from scripts.experiments.trajectories.tools import process
+
+
+modulename = __file__.split(os.sep)[-1].split('.')[-2]
+
+if __name__ == "__main__":
+    sigma_q = 1.
+    sigma_p = 1.
+    Ds = 2 ** np.arange(7)
+    num_repetitions = 10
+    N = 500
+    lmbda = 0.0001
+    m = N
+    num_steps = 100
+    max_steps = 1000
+    step_size = .1
+    
+    sigma0 = 0.5
+    lmbda0 = 0.0001
+    
+    job_generator = lambda D : GaussianTrajectoryJob(N, D, m, sigma_q, sigma_p,
+                                                     num_steps, step_size,
+                                                     sigma0, lmbda0, max_steps)
+    
+    process(modulename, job_generator, Ds, num_repetitions, N, lmbda, num_steps,
+            step_size, max_steps, compute_local=True)
