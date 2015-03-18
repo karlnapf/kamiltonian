64d059c374826dcd3bc501d0e6f1e4bb8f100020
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-03-18 15:55:23 +0000

    some incomplete work on safe averaging of log-prbabilities

--- kmc/tools/numerics.py
+++ kmc/tools/numerics.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def log_sum_exp(X):
     """
     Computes log sum_i exp(X_i).
@@ -7,10 +8,10 @@ def log_sum_exp(X):
     where you have samples from p(x) and can compute log f(x)
     """
     # extract minimum
-    X0=X.min()
-    X_without_X0=np.delete(X,X.argmin())
+    X0 = X.min()
+    X_without_X0 = np.delete(X, X.argmin())
     
-    return X0+np.log(1+np.sum(np.exp(X_without_X0-X0)))
+    return X0 + np.log(1 + np.sum(np.exp(X_without_X0 - X0)))
 
 def log_mean_exp(X):
     """
@@ -19,4 +20,27 @@ def log_mean_exp(X):
     where you have samples from p(x) and can compute log f(x)
     """
     
-    return log_sum_exp(X)-np.log(len(X))
+    return log_sum_exp(X) - np.log(len(X))
+
+def avg_prob_of_log_probs(X):
+    """
+    Given a set of log-probabilities, this computes log-mean-exp of them.
+    Careful checking is done to prevent buffer overflows
+    Similar to calling (but overflow-safe): log_mean_exp(log_prob)
+    """
+    
+    # extract inf inds (no need to delete X0 from X here)
+    X0 = X.min()
+    inf_inds = np.isinf(np.exp(X - X0))
+    
+    # remove these numbers
+    X_without_inf = X[~inf_inds]
+    
+    # return exp-log-mean-exp on shortened array
+    avg_prob_without_inf = np.exp(log_mean_exp(X_without_inf))
+    
+    # re-normalise by the full length, which is equivalent to adding a zero probability observation
+    renormaliser = float(len(X_without_inf)) / len(X)
+    avg_prob_without_inf = avg_prob_without_inf * renormaliser
+    
+    return avg_prob_without_inf
