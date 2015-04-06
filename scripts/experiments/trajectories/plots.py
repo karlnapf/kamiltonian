e4d43798441f7c2148ce814f2640cded199cade6
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-06 13:38:51 +0100

    new heatmap plot

--- scripts/experiments/trajectories/plots.py
+++ scripts/experiments/trajectories/plots.py
@@ -31,6 +31,8 @@ def plot_trajectory_result_mean_median_fixed_N(fname, N):
     plt.xscale("log")
     plt.grid(True)
     plt.xlim([Ds.min(), Ds.max()])
+    plt.xlabel(r"$d$")
+    plt.title(r"N=%d" % N)
     ylim = plt.ylim()
     plt.ylim([ylim[0], 1.01])
     
@@ -69,10 +71,43 @@ def plot_trajectory_result_mean_median_fixed_D(fname, D):
     plt.xscale("log")
     plt.grid(True)
     plt.xlim([Ns.min(), Ns.max()])
+    plt.xlabel(r"$n$")
     ylim = plt.ylim()
     plt.ylim([ylim[0], 1.01])
+    plt.title(r"d=%d" % D)
     
     plt.legend(["HMC", "KMC median", "KMC 25\%-75\%", "KMC 5\%-95\%"], loc="lower left")
     fname_base = fname.split(".")[-2]
     plt.savefig(fname_base + "_D=%d.eps" % D, axis_inches='tight')
+    plt.show()
+
+def plot_acceptance_heatmap(Ns, Ds, acc):
+    plt.pcolor(Ns, Ds, acc)
+    plt.yscale("log")
+    plt.xlim([np.min(Ns), np.max(Ns)])
+    plt.ylim([np.min(Ds), np.max(Ds)])
+    plt.xlabel(r"$n$")
+    plt.ylabel(r"$d$")
+    plt.colorbar()
+
+def plot_trajectory_result_heatmap(fname):
+    with open(fname, 'r') as f:
+        # created as: avg_accept = np.zeros((num_repetitions, len(Ds), len(Ns)))
+        results = np.load(f)
+        Ns = results['Ns']
+        Ds = results["Ds"]
+        avg_accept = np.mean(results["avg_accept"], 0)
+        avg_accept_est = np.mean(results["avg_accept_est"], 0)
+    
+    plt.figure()
+    plot_acceptance_heatmap(Ns, Ds, avg_accept)
+    fname_base = fname.split(".")[-2]
+    plt.savefig(fname_base + "_hmc.eps", axis_inches='tight')
+    
+    
+    plt.figure()
+    plot_acceptance_heatmap(Ns, Ds, avg_accept_est)
+    fname_base = fname.split(".")[-2]
+    plt.savefig(fname_base + "_kmc.eps", axis_inches='tight')
+    
     plt.show()
