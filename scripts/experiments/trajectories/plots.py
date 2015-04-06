ace2c83ee2219cfc0538588abb124a34c27dc954
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-06 11:18:50 +0100

    updated plotting ocde

--- scripts/experiments/trajectories/plots.py
+++ scripts/experiments/trajectories/plots.py
@@ -1,15 +1,20 @@
 from kmc.tools.latex_plot_init import plt
 import numpy as np
 
-
-def plot_trajectory_result_mean_median(fname):
+def plot_trajectory_result_mean_median_fixed_N(fname, N):
     with open(fname, 'r') as f:
-            results = np.load(f)
-            avg_accept = results["avg_accept"]
-            avg_accept_est = results["avg_accept_est"]
-            vols = results["vols"]
-            vols_est = results["vols_est"]
-            Ds = results["Ds"]
+        # created as: avg_accept = np.zeros((num_repetitions, len(Ds), len(Ns)))
+        results = np.load(f)
+        Ns = results['Ns']
+        if not N in Ns:
+            raise ValueError("Provided N (%d) is not in experiment" % N)
+        
+        N_idx = np.where(Ns==N)[0][0]
+        Ds = results["Ds"]
+        avg_accept = results["avg_accept"][:,:,N_idx]
+        avg_accept_est = results["avg_accept_est"][:,:,N_idx]
+        vols = results["vols"][:,:,N_idx]
+        vols_est = results["vols_est"][:,:,N_idx]
     
     plt.figure()
     plt.plot(Ds, np.median(avg_accept, 0), 'r')
@@ -23,9 +28,6 @@ def plot_trajectory_result_mean_median(fname):
     plt.plot(Ds, np.percentile(avg_accept_est, 75, 0), 'b-.')
     plt.plot(Ds, np.median(avg_accept, 0), 'r')
     
-#     plt.plot(Ds, np.percentile(avg_accept, 5, 0), 'r--')
-#     plt.plot(Ds, np.percentile(avg_accept, 95, 0), 'r--')
-    
     plt.xscale("log")
     plt.grid(True)
     plt.xlim([Ds.min(), Ds.max()])
@@ -34,51 +36,43 @@ def plot_trajectory_result_mean_median(fname):
     
     plt.legend(["HMC", "KMC median", "KMC 25\%-75\%", "KMC 5\%-95\%"], loc="lower left")
     fname_base = fname.split(".")[-2]
-    plt.savefig(fname_base + ".eps", axis_inches='tight')
+    plt.savefig(fname_base + "_N=%d.eps" % N, axis_inches='tight')
     plt.show()
     
-def plot_trajectory_result_boxplot(fname):
+def plot_trajectory_result_mean_median_fixed_D(fname, D):
     with open(fname, 'r') as f:
-            results = np.load(f)
-            avg_accept = results["avg_accept"]
-            avg_accept_est = results["avg_accept_est"]
-            Ds = results["Ds"]
+        # created as: avg_accept = np.zeros((num_repetitions, len(Ds), len(Ns)))
+        results = np.load(f)
+        Ns = results['Ns']
+        Ds = results["Ds"]
+        if not D in Ds:
+            raise ValueError("Provided D (%d) is not in experiment" % D)
+        
+        D_idx = np.where(Ds==D)[0][0]
+        avg_accept = results["avg_accept"][:,D_idx,:]
+        avg_accept_est = results["avg_accept_est"][:,D_idx,:]
+        vols = results["vols"][:,D_idx,:]
+        vols_est = results["vols_est"][:,D_idx,:]
     
     plt.figure()
-    plt.boxplot(avg_accept_est)
+    plt.plot(Ns, np.median(avg_accept, 0), 'r')
+    plt.plot(Ns, np.median(avg_accept_est, 0), 'b')
+    plt.plot(Ns, np.percentile(avg_accept_est, 25, 0), 'b-.')
+    plt.plot(Ns, np.percentile(avg_accept_est, 5, 0), color="grey")
+    plt.plot(Ns, np.percentile(avg_accept_est, 95, 0), color="grey")
+    plt.fill_between(Ns, np.percentile(avg_accept_est, 5, 0),
+                     np.percentile(avg_accept_est, 95, 0),
+                     color="grey", alpha=.5)
+    plt.plot(Ns, np.percentile(avg_accept_est, 75, 0), 'b-.')
+    plt.plot(Ns, np.median(avg_accept, 0), 'r')
+    
+    plt.xscale("log")
+    plt.grid(True)
+    plt.xlim([Ns.min(), Ns.max()])
     ylim = plt.ylim()
     plt.ylim([ylim[0], 1.01])
-    plt.xticks(np.arange(1, len(Ds) + 1), [str(D) for D in Ds])
-    plt.grid(True)
-    plt.figure()
-    plt.boxplot(avg_accept)
-    plt.ylim([ylim[0], 1.01])
-    plt.xticks(np.arange(1, len(Ds) + 1), [str(D) for D in Ds])
-    plt.grid(True)
-    fname_base = fname.split(".")[-2]
-    plt.savefig(fname_base + ".eps", axis_inches='tight')
-    plt.show()
     
-
-def plot_trajectory_result_boxplot_mix(fname):
-    with open(fname, 'r') as f:
-            results = np.load(f)
-            avg_accept = results["avg_accept"]
-            avg_accept_est = results["avg_accept_est"]
-            Ds = results["Ds"]
-    
-    plt.figure()
-    plt.plot(np.log2(Ds) + 1, np.mean(avg_accept, 0), 'ro')
-    plt.boxplot(avg_accept_est)
-    ylim = plt.ylim()
-    plt.ylim([ylim[0], 1.01])
-    plt.xticks(np.arange(1, len(Ds) + 1), [r"$2^{"+str(np.int(np.log2(D))) + "}$" for D in Ds])
-    plt.gca().yaxis.grid(True)
-    plt.plot(np.log2(Ds) + 1, np.mean(avg_accept, 0), 'ro')
-#     plt.plot(np.log2(Ds)+1, np.percentile(avg_accept, 5, 0), 'r--')
-    plt.legend(["HMC"], loc="lower left")
-    plt.ylabel("Acceptance probability")
-    plt.xlabel(r"$D$")
+    plt.legend(["HMC", "KMC median", "KMC 25\%-75\%", "KMC 5\%-95\%"], loc="lower left")
     fname_base = fname.split(".")[-2]
-    plt.savefig(fname_base + ".eps", axis_inches='tight')
+    plt.savefig(fname_base + "_D=%d.eps" % D, axis_inches='tight')
     plt.show()
