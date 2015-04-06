7c99a0f97f23c5854106f4826cbc6e4dd67ca2bf
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-06 13:38:40 +0100

    no more plots and final experiment

--- scripts/experiments/trajectories/random_feats/average_accept_gaussian_target.py
+++ scripts/experiments/trajectories/random_feats/average_accept_gaussian_target.py
@@ -1,10 +1,7 @@
 import os
 
-from kmc.tools.Log import logger
 import numpy as np
 from scripts.experiments.trajectories.independent_jobs_classes.random_feats.GaussianTrajectoryJob import GaussianTrajectoryJob
-from scripts.experiments.trajectories.plots import plot_trajectory_result_mean_median_fixed_N,\
-    plot_trajectory_result_mean_median_fixed_D
 from scripts.experiments.trajectories.tools import process
 
 
@@ -13,8 +10,8 @@ modulename = __file__.split(os.sep)[-1].split('.')[-2]
 if __name__ == "__main__":
     sigma_q = 1.
     sigma_p = 1.
-    Ds = np.flipud(2 ** np.arange(8))
-    Ns = np.flipud(np.array([50, 100, 200, 500, 1000, 2000]))
+    Ds = np.sort(2 ** np.arange(8))[::-1]
+    Ns = np.sort([50, 100, 200, 500, 1000, 2000])[::-1]
     print(Ns)
     print(Ds)
     num_repetitions = 30
@@ -31,8 +28,3 @@ if __name__ == "__main__":
     
     process(modulename, job_generator, Ds, Ns, num_repetitions, num_steps,
             step_size, max_steps, compute_local=False)
-
-    fname = modulename + ".npy"
-    plot_trajectory_result_mean_median_fixed_N(fname, N=Ns[0])
-    plot_trajectory_result_mean_median_fixed_D(fname, D=Ds[0])
-    
