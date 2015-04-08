ae81f2465e23f588126bef2470e6ed859df80ba6
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-08 19:08:11 +0100

    added some first plots

--- scripts/experiments/trajectories/plots.py
+++ scripts/experiments/trajectories/plots.py
@@ -1,5 +1,7 @@
+from kmc.tools.Log import logger
 from kmc.tools.latex_plot_init import plt
 import numpy as np
+
 
 def plot_trajectory_result_mean_median_fixed_N(fname, N):
     with open(fname, 'r') as f:
@@ -9,12 +11,12 @@ def plot_trajectory_result_mean_median_fixed_N(fname, N):
         if not N in Ns:
             raise ValueError("Provided N (%d) is not in experiment" % N)
         
-        N_idx = np.where(Ns==N)[0][0]
+        N_idx = np.where(Ns == N)[0][0]
         Ds = results["Ds"]
-        avg_accept = results["avg_accept"][:,:,N_idx]
-        avg_accept_est = results["avg_accept_est"][:,:,N_idx]
-        vols = results["vols"][:,:,N_idx]
-        vols_est = results["vols_est"][:,:,N_idx]
+        avg_accept = results["avg_accept"][:, :, N_idx]
+        avg_accept_est = results["avg_accept_est"][:, :, N_idx]
+        vols = results["vols"][:, :, N_idx]
+        vols_est = results["vols_est"][:, :, N_idx]
     
     plt.figure()
     plt.plot(Ds, np.median(avg_accept, 0), 'r')
@@ -32,14 +34,13 @@ def plot_trajectory_result_mean_median_fixed_N(fname, N):
     plt.grid(True)
     plt.xlim([Ds.min(), Ds.max()])
     plt.xlabel(r"$d$")
-    plt.title(r"N=%d" % N)
+    plt.title(r"n=%d" % N)
     ylim = plt.ylim()
     plt.ylim([ylim[0], 1.01])
     
     plt.legend(["HMC", "KMC median", "KMC 25\%-75\%", "KMC 5\%-95\%"], loc="lower left")
     fname_base = fname.split(".")[-2]
     plt.savefig(fname_base + "_N=%d.eps" % N, axis_inches='tight')
-    plt.show()
     
 def plot_trajectory_result_mean_median_fixed_D(fname, D):
     with open(fname, 'r') as f:
@@ -50,11 +51,11 @@ def plot_trajectory_result_mean_median_fixed_D(fname, D):
         if not D in Ds:
             raise ValueError("Provided D (%d) is not in experiment" % D)
         
-        D_idx = np.where(Ds==D)[0][0]
-        avg_accept = results["avg_accept"][:,D_idx,:]
-        avg_accept_est = results["avg_accept_est"][:,D_idx,:]
-        vols = results["vols"][:,D_idx,:]
-        vols_est = results["vols_est"][:,D_idx,:]
+        D_idx = np.where(Ds == D)[0][0]
+        avg_accept = results["avg_accept"][:, D_idx, :]
+        avg_accept_est = results["avg_accept_est"][:, D_idx, :]
+        vols = results["vols"][:, D_idx, :]
+        vols_est = results["vols_est"][:, D_idx, :]
     
     plt.figure()
     plt.plot(Ns, np.median(avg_accept, 0), 'r')
@@ -76,10 +77,9 @@ def plot_trajectory_result_mean_median_fixed_D(fname, D):
     plt.ylim([ylim[0], 1.01])
     plt.title(r"d=%d" % D)
     
-    plt.legend(["HMC", "KMC median", "KMC 25\%-75\%", "KMC 5\%-95\%"], loc="lower left")
+#     plt.legend(["HMC", "KMC median", "KMC 25\%-75\%", "KMC 5\%-95\%"], loc="lower left")
     fname_base = fname.split(".")[-2]
     plt.savefig(fname_base + "_D=%d.eps" % D, axis_inches='tight')
-    plt.show()
 
 def plot_acceptance_heatmap(Ns, Ds, acc):
     plt.pcolor(Ns, Ds, acc)
@@ -96,18 +96,40 @@ def plot_trajectory_result_heatmap(fname):
         results = np.load(f)
         Ns = results['Ns']
         Ds = results["Ds"]
-        avg_accept = np.mean(results["avg_accept"], 0)
         avg_accept_est = np.mean(results["avg_accept_est"], 0)
     
     plt.figure()
-    plot_acceptance_heatmap(Ns, Ds, avg_accept)
+    plot_acceptance_heatmap(Ns, Ds, avg_accept_est)
+    plt.xscale('log')
     fname_base = fname.split(".")[-2]
-    plt.savefig(fname_base + "_hmc.eps", axis_inches='tight')
+    plt.savefig(fname_base + "_kmc.eps", axis_inches='tight')
+
+def plot_trajectory_result_necessary_data(fname, accs_at_least=[0.5]):
+    with open(fname, 'r') as f:
+        # created as: avg_accept = np.zeros((num_repetitions, len(Ds), len(Ns)))
+        results = np.load(f)
+        Ns = results['Ns']
+        Ds = results["Ds"]
+        avg_accept_est = np.mean(results["avg_accept_est"], 0)
     
     
     plt.figure()
-    plot_acceptance_heatmap(Ns, Ds, avg_accept_est)
-    fname_base = fname.split(".")[-2]
-    plt.savefig(fname_base + "_kmc.eps", axis_inches='tight')
+    for acc_at_least in accs_at_least:
+        N_at_least = np.zeros(len(Ds))
+        for i, D in enumerate(Ds):
+            w = np.where(avg_accept_est[i,:]>acc_at_least)[0]
+            if len(w)>0:
+                N_at_least[i] = np.min(Ns[w])
+                logger.info("%.2f acc. for D=%d at N=%d" % (acc_at_least, D, N_at_least[i]))
+            else:
+                logger.info("Did not reach %.2f acc. for D=%d" % (acc_at_least, D))
+            
+        plt.plot(Ds, N_at_least)
+    plt.yscale('log')
+    plt.xscale('log')
     
-    plt.show()
+    plt.legend(["%.2f acc." % acc_at_least for acc_at_least in accs_at_least], loc="lower right")
+    plt.grid(True)
+    
+    fname_base = fname.split(".")[-2]
+    plt.savefig(fname_base + "_data_needs_kmc.eps", axis_inches='tight')
--- scripts/experiments/trajectories/random_feats/average_accept_gaussian_target_local.py
+++ scripts/experiments/trajectories/random_feats/average_accept_gaussian_target_local.py
@@ -1,8 +1,13 @@
 import os
 
 from kmc.tools.Log import logger
+import matplotlib.pyplot as plt
 import numpy as np
 from scripts.experiments.trajectories.independent_jobs_classes.random_feats.GaussianTrajectoryJob import GaussianTrajectoryJob
+from scripts.experiments.trajectories.plots import plot_trajectory_result_heatmap, \
+    plot_trajectory_result_mean_median_fixed_N, \
+    plot_trajectory_result_mean_median_fixed_D,\
+    plot_trajectory_result_necessary_data
 from scripts.experiments.trajectories.tools import process
 
 
@@ -12,11 +17,12 @@ if __name__ == "__main__":
     logger.setLevel(10)
     sigma_q = 1.
     sigma_p = 1.
-    Ds = np.flipud(2 ** np.arange(8))
-    Ns = np.flipud(np.array([50, 100, 200, 500, 1000, 2000]))
+    Ds = np.sort(2 ** np.arange(8))[::-1]
+    Ns = np.sort([50, 100, 200, 500, 1000, 2000, 3000, 5000, 8000, 10000])[::-1]
+    
     print(Ns)
     print(Ds)
-    num_repetitions = 10
+    num_repetitions = 50
     num_steps = 100
     max_steps = 1000
     step_size = .1
@@ -30,3 +36,17 @@ if __name__ == "__main__":
     
     process(modulename, job_generator, Ds, Ns, num_repetitions, num_steps,
             step_size, max_steps, compute_local=True)
+
+
+    fname = modulename + ".npy"
+    fname = fname.replace("_local", "")
+    plot_trajectory_result_heatmap(fname)
+      
+    # slices of heatmap with confidence intervals
+    for N in Ns:
+        plot_trajectory_result_mean_median_fixed_N(fname, N=N)
+          
+    for D in Ds:
+        plot_trajectory_result_mean_median_fixed_D(fname, D=D)
+    
+    plot_trajectory_result_necessary_data(fname, [0.1, 0.3, 0.5, 0.8])
