c8762999ac2647e4487bc81e8d1719d735b83046
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-08 12:25:55 +0100

    more repetitions

--- scripts/experiments/trajectories/random_feats/average_accept_gaussian_target.py
+++ scripts/experiments/trajectories/random_feats/average_accept_gaussian_target.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Ns = np.sort([50, 100, 200, 500, 1000, 2000])[::-1]
     print(Ns)
     print(Ds)
-    num_repetitions = 30
+    num_repetitions = 100
     num_steps = 100
     max_steps = 1000
     step_size = .1
