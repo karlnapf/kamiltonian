e8a7d12110ef0993c8a3362d0da04ba72c45fd7e
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-14 16:16:15 +0100

    learn parameters

--- scripts/experiments/trajectories/random_feats/laplace_target/average_accept_local.py
+++ scripts/experiments/trajectories/random_feats/laplace_target/average_accept_local.py
@@ -12,12 +12,8 @@ if __name__ == "__main__":
     logger.setLevel(20)
     scale_q = 1.
     sigma_p = 1.
-    Ds = np.sort(2 ** np.arange(5))[::-1]
-    Ns = np.sort([50, 100, 200, 500, 1000])[::-1]#, 2000, 5000, 10000])[::-1]
-    
-    Ds = np.array([2.])
-    Ns = np.array([500])
-    
+    Ds = np.sort(2 ** np.arange(8))[::-1]
+    Ns = np.sort([50, 100, 200, 500, 1000, 2000, 5000, 10000])[::-1]
     
     print(Ns)
     print(Ds)
@@ -32,7 +28,8 @@ if __name__ == "__main__":
     job_generator = lambda D, N, m : LaplaceTrajectoryJob(N, D, m, scale_q,
                                                           sigma_p, num_steps,
                                                           step_size, scale0,
-                                                          lmbda0, max_steps)
+                                                          lmbda0, max_steps,
+                                                          learn_parameters=True)
     
     process(modulename, job_generator, Ds, Ns, num_repetitions, num_steps,
             step_size, max_steps, compute_local=True)
