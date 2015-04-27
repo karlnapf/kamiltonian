e7854ff95fe6aabcf23fa4b793dfabdd0d243664
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-27 18:18:32 +0100

    added single point log acceptance probability computation

--- kmc/hamiltonian/hamiltonian.py
+++ kmc/hamiltonian/hamiltonian.py
@@ -11,6 +11,11 @@ def compute_log_accept_pr(q0, p0, Qs, Ps, logq, logp):
     
     return np.minimum(np.zeros(H.shape), -H + H0)
 
+def compute_log_accept_pr_single(q0, p0, q, p, logq, logp):
+    H0 = compute_hamiltonian(q0[np.newaxis, :], p0[np.newaxis, :], logq, logp)[0]
+    H = compute_hamiltonian(q[np.newaxis, :], p[np.newaxis, :], logq, logp)[0]
+    return np.minimum(0., -H + H0)
+
 def compute_log_det_trajectory(Qs, Ps):
     joint = np.hstack((Qs, Ps))
     Sigma = np.cov(joint.T) 
