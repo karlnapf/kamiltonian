2f0b50e2d2afda33388bc9c501b0fc00d3bfa3bd
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-04-27 18:18:44 +0100

    added leapfrog with no storing of path

--- kmc/hamiltonian/leapfrog.py
+++ kmc/hamiltonian/leapfrog.py
@@ -6,8 +6,8 @@ def leapfrog(q, dlogq, p, dlogp, step_size=0.3, num_steps=1):
     Qs = np.zeros(Ps.shape)
     
     # create copy of state
-    p = np.array(p)
-    q = np.array(q)
+    p = np.array(p.copy())
+    q = np.array(q.copy())
     Ps[0] = p
     Qs[0] = q
     
@@ -33,3 +33,27 @@ def leapfrog(q, dlogq, p, dlogp, step_size=0.3, num_steps=1):
             p = p - (step_size / 2) * -dlogq_eval
 
     return Qs, Ps
+
+def leapfrog_no_storing(q, dlogq, p, dlogp, step_size=0.3, num_steps=1):
+    # create copy of state
+    p = np.array(p.copy())
+    q = np.array(q.copy())
+    
+    # half momentum update
+    p = p - (step_size / 2) * -dlogq(q)
+    
+    # alternate full variable and momentum updates
+    for i in range(num_steps):
+        q = q + step_size * -dlogp(p)
+
+        # precompute since used for two half-steps
+        dlogq_eval = dlogq(q)
+
+        #  first half momentum update
+        p = p - (step_size / 2) * -dlogq_eval
+        
+        # second half momentum update
+        if i != num_steps - 1:
+            p = p - (step_size / 2) * -dlogq_eval
+
+    return q, p
