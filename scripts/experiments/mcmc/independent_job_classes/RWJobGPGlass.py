aff8dcaead58984048fe58e9c8f3d028e0695eeb
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-05-11 10:19:23 +0100

    ground truth job for GP glass posterior

---
+++ scripts/experiments/mcmc/independent_job_classes/RWJobGPGlass.py
@@ -0,0 +1,32 @@
+from kameleon_mcmc.gp.GPData import GPData
+
+from kmc.densities.gp_classification_posterior_ard import prior_log_pdf,\
+    PseudoMarginalHyperparameters
+from kmc.tools.Log import logger
+import numpy as np
+import scipy as sp
+from scripts.experiments.mcmc.independent_job_classes.RWJob import RWJob
+
+class RWJobGPGlass(RWJob):
+    def __init__(self, num_iterations, start,
+                 sigma_proposal,
+                 statistics = {}, num_warmup=500, thin_step=1):
+        
+        # load data using kameleon-mcmc code
+        logger.info("Loading data")
+        X,y=GPData.get_glass_data()
+    
+        # normalise and whiten dataset, as done in kameleon-mcmc code
+        logger.info("Whitening data")
+        X-=np.mean(X, 0)
+        L=np.linalg.cholesky(np.cov(X.T))
+        X=sp.linalg.solve_triangular(L, X.T, lower=True).T
+        
+        # build target, as in kameleon-mcmc code
+        prior = prior_log_pdf
+        n_importance = 100
+        ridge = 1e-3 
+        target = PseudoMarginalHyperparameters(X, y, n_importance, prior, ridge, num_shogun_threads=1)
+        
+        RWJob.__init__(self, target, num_iterations, start, sigma_proposal,
+                       statistics, num_warmup, thin_step)
---
+++ scripts/experiments/mcmc/random_feats/gp_target/mcmc_gp_target_ground_truth.py
@@ -0,0 +1,86 @@
+from collections import OrderedDict
+import os
+import pickle
+import uuid
+
+from independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
+from independent_jobs.engines.SerialComputationEngine import SerialComputationEngine
+from independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
+from independent_jobs.tools.FileSystem import FileSystem
+
+from kmc.tools.Log import logger
+from kmc.tools.convergence_stats import avg_ess
+import numpy as np
+from scripts.experiments.mcmc.independent_job_classes.RWJobGPGlass import RWJobGPGlass
+
+
+modulename = __file__.split(os.sep)[-1].split('.')[-2]
+statistics = OrderedDict()
+statistics['avg_ess'] = avg_ess
+
+def rw_generator_isotropic(num_warmup, thin_step):
+    # tuned towards roughly 23% acceptance
+    sigma_proposal = 0.54
+    
+    start = np.zeros(9)
+    
+    return RWJobGPGlass(num_iterations,
+                        start, sigma_proposal,
+                        statistics, num_warmup, thin_step)
+
+if __name__ == "__main__":
+    logger.setLevel(10)
+    num_repetitions = 10
+    num_repetitions = 1
+    
+    # plain MCMC parameters
+    thin_step = 1
+    num_iterations = 500
+    num_warmup = 0
+    
+    compute_local = False
+    
+    if not FileSystem.cmd_exists("sbatch") or compute_local:
+        engine = SerialComputationEngine()
+        
+    else:
+        johns_slurm_hack = "#SBATCH --partition=intel-ivy,wrkstn,compute"
+        folder = os.sep + os.sep.join(["nfs", "data3", "ucabhst", modulename])
+        batch_parameters = BatchClusterParameters(foldername=folder,
+                                                  resubmit_on_timeout=False,
+                                                  parameter_prefix=johns_slurm_hack)
+        engine = SlurmComputationEngine(batch_parameters, check_interval=1,
+                                        do_clean_up=True)
+        engine.max_jobs_in_queue = 1000
+        engine.store_fire_and_forget = True
+    
+    aggs_rw = []
+    
+    for i in range(num_repetitions):
+        job = rw_generator_isotropic(num_warmup, thin_step)
+        logger.info("Repetition %d/%d, %s" % (i + 1, num_repetitions, job.get_parameter_fname_suffix()))
+        aggs_rw += [engine.submit_job(job)]
+        
+    engine.wait_for_all()
+    
+    for i, agg in enumerate(aggs_rw):
+        agg.finalize()
+        mcmc_job = agg.get_final_result().mcmc_job
+        agg.clean_up()
+        
+        # print some summary stats
+        accepted = mcmc_job.accepted
+        avg_ess = mcmc_job.posterior_statistics["avg_ess"]
+        
+        time = mcmc_job.time_taken_sampling
+        time_set_up = mcmc_job.time_taken_set_up
+    
+        logger.info("Repetition %d" % i)
+        logger.info("Average acceptance probability: %.2f" % np.mean(accepted))
+        logger.info("Average ESS: %.2f" % avg_ess)
+        
+        # save result under unique filename
+        fname = "%s_ground_truth_iterations=%d_%s" % (modulename, num_iterations, unicode(uuid.uuid4()))
+        with open(fname, 'w+') as f:
+            logger.info("Storing result under %s" % fname)
+            pickle.dump(mcmc_job, f)
