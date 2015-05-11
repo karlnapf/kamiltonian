1ad098f40efd4c1850230481a9182e70cadaa385
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-05-11 11:30:46 +0100

    construct target in job itself

--- scripts/experiments/mcmc/independent_job_classes/RWJobGPGlass.py
+++ scripts/experiments/mcmc/independent_job_classes/RWJobGPGlass.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 from kameleon_mcmc.gp.GPData import GPData
 
 from kmc.densities.gp_classification_posterior_ard import prior_log_pdf,\
@@ -7,11 +9,17 @@ import numpy as np
 import scipy as sp
 from scripts.experiments.mcmc.independent_job_classes.RWJob import RWJob
 
+
 class RWJobGPGlass(RWJob):
     def __init__(self, num_iterations, start,
                  sigma_proposal,
                  statistics = {}, num_warmup=500, thin_step=1):
+        # target not constructed yet
+        RWJob.__init__(self, None, num_iterations, start, sigma_proposal,
+               statistics, num_warmup, thin_step)
         
+    @abstractmethod
+    def set_up(self):
         # load data using kameleon-mcmc code
         logger.info("Loading data")
         X,y=GPData.get_glass_data()
@@ -26,7 +34,4 @@ class RWJobGPGlass(RWJob):
         prior = prior_log_pdf
         n_importance = 100
         ridge = 1e-3 
-        target = PseudoMarginalHyperparameters(X, y, n_importance, prior, ridge, num_shogun_threads=1)
-        
-        RWJob.__init__(self, target, num_iterations, start, sigma_proposal,
-                       statistics, num_warmup, thin_step)
+        self.target = PseudoMarginalHyperparameters(X, y, n_importance, prior, ridge, num_shogun_threads=1)
