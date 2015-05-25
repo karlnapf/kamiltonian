1eb5c42d97639a8b94208c77f0df650d07c2f543
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-05-25 16:04:33 +0100

    initial work on abc example

---
+++ kmc/densities/abc_skew_normal.py
@@ -0,0 +1,83 @@
+from rpy2 import robjects
+import rpy2
+from rpy2.robjects.numpy2ri import ri2numpy
+
+from kmc.densities.abc import ABCPosterior
+from kmc.densities.gaussian import log_gaussian_pdf
+from kmc.tools.Log import logger
+import numpy as np
+
+
+robjects.r('library(sn)')
+rpy2.robjects.numpy2ri.activate()
+
+
+def sample_skew_normal(N, mu=np.zeros(2), Sigma=np.eye(2), alphas = np.ones(2)):
+    D = len(mu)
+    assert len(mu.shape) == 1
+    assert len(alphas.shape) == 1
+    assert len(Sigma.shape) == 2
+    
+    assert D == Sigma.shape[0]
+    assert D == Sigma.shape[1]
+    assert D == len(alphas)
+    
+#     rdata = sn.rmsn(N, mu, Sigma, alphas)
+    
+    
+    r_rmsn = robjects.r['rmsn']
+    r_alpha = robjects.FloatVector(alphas)
+    r_xi = robjects.FloatVector(mu)
+    r_Sigma_vec = robjects.FloatVector(Sigma.transpose().reshape(Sigma.size))
+    r_Omega = robjects.r.matrix(r_Sigma_vec, nrow=D, ncol=D)
+    
+    result = r_rmsn(N, r_xi, r_Omega, r_alpha)
+    return ri2numpy(result)
+
+def wide_zero_mean_normal_prior(x):
+    D = len(x)
+    mu = np.zeros(D)
+    Sigma = np.eye(D) * 5
+    return log_gaussian_pdf(x, mu, Sigma, is_cholesky=True)
+
+def skew_normal_simulator(theta):
+    D = len(theta)
+    Sigma = np.eye(D)
+    alphas = np.zeros(D) + 10
+    N = 10
+    
+    return sample_skew_normal(N, theta, Sigma, alphas)
+
+class ABCSkewNormalPosterior(ABCPosterior):
+    def __init__(self, D=2, n_lik_samples=10, epsilon=4., prior=wide_zero_mean_normal_prior):
+        
+        ABCPosterior.__init__(self, skew_normal_simulator, n_lik_samples, epsilon, prior)
+        self.D = D
+        
+    def set_up(self):
+        logger.info("Generating dataset")
+        # true vale of theta
+        theta = np.zeros(self.D)
+        
+        old = np.random.get_state()
+        np.random.seed(0)
+        self.data = skew_normal_simulator(theta)
+        np.random.set_state(old)
+    
+
+if __name__ == "__main__":
+    N=1000
+    D=2
+    mu = np.zeros(D)
+    Sigma = np.eye(D)
+    alphas = np.zeros(D) + 10
+    samples = sample_skew_normal(N, mu, Sigma, alphas)
+    
+    import seaborn as sns
+    sns.kdeplot(samples)
+    
+    m = np.mean(samples, 0)
+    sns.plt.plot([m[0]], [m[1]], 'x', markersize=15)
+    
+    sns.plt.show()
+    
---
+++ scripts/experiments/mcmc/abc_target/ground_truth/mcmc_abc_skew_normal_target.py
@@ -0,0 +1,100 @@
+from collections import OrderedDict
+import os
+
+from independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
+from independent_jobs.engines.SerialComputationEngine import SerialComputationEngine
+from independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
+from independent_jobs.tools.FileSystem import FileSystem
+
+from kmc.densities.abc_skew_normal import ABCSkewNormalPosterior
+from kmc.tools.Log import logger
+from kmc.tools.convergence_stats import avg_ess, min_ess
+import numpy as np
+from scripts.experiments.mcmc.independent_job_classes.MCMCJob import MCMCJobResultAggregatorStoreHome
+from scripts.experiments.mcmc.independent_job_classes.RWJob import RWJob
+from scripts.experiments.mcmc.independent_job_classes.debug import plot_mcmc_result
+
+
+modulename = __file__.split(os.sep)[-1].split('.')[-2]
+statistics = OrderedDict()
+statistics['avg_ess'] = avg_ess
+statistics['min_ess'] = min_ess
+
+def rw_generator_isotropic(num_warmup, thin_step):
+    # tuned towards roughly 23% acceptance
+#     sigma_proposal = 0.3 # correct pm sampler
+    sigma_proposal = .7 # marginal sampler
+    
+    start = np.zeros(2)
+    
+    target = ABCSkewNormalPosterior()
+    job = RWJob(target, num_iterations,
+                        start, sigma_proposal,
+                        statistics, num_warmup, thin_step)
+    
+    # marginal sampler
+    job.recompute_log_pdf = True
+    
+    job.walltime = 60 * 60
+    
+    # store results in home dir straight away
+    d = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1]) + os.sep
+    job.aggregator = MCMCJobResultAggregatorStoreHome(d)
+    
+    return job
+
+if __name__ == "__main__":
+    logger.setLevel(10)
+    num_repetitions = 100
+    
+    # plain MCMC parameters, plan is to use every 200th sample
+    thin_step = 1
+    num_iterations = 5000
+    num_warmup = 1000
+    
+    compute_local = False
+    
+    if not FileSystem.cmd_exists("sbatch") or compute_local:
+        engine = SerialComputationEngine()
+        
+    else:
+        johns_slurm_hack = "#SBATCH --partition=intel-ivy,wrkstn,compute"
+        folder = os.sep + os.sep.join(["nfs", "data3", "ucabhst", modulename])
+        batch_parameters = BatchClusterParameters(foldername=folder,
+                                                  resubmit_on_timeout=False,
+                                                  parameter_prefix=johns_slurm_hack)
+        engine = SlurmComputationEngine(batch_parameters, check_interval=1,
+                                        do_clean_up=True)
+        engine.max_jobs_in_queue = 1000
+        engine.store_fire_and_forget = True
+    
+    aggs = []
+    
+    for i in range(num_repetitions):
+        job = rw_generator_isotropic(num_warmup, thin_step)
+        logger.info("Repetition %d/%d, %s" % (i + 1, num_repetitions, job.get_parameter_fname_suffix()))
+        aggs += [engine.submit_job(job)]
+        
+    engine.wait_for_all()
+    
+    for i, agg in enumerate(aggs):
+        agg.finalize()
+        result = agg.get_final_result()
+        agg.clean_up()
+
+        if isinstance(engine, SerialComputationEngine):
+            plot_mcmc_result(result, D1=0, D2=1)
+            agg.store_fire_and_forget_result(folder="", job_name="")
+            
+        
+        # print some summary stats
+        accepted = result.accepted
+        avg_ess = result.posterior_statistics["avg_ess"]
+        
+        time = result.time_taken_sampling
+        time_set_up = result.time_taken_set_up
+    
+        logger.info("Repetition %d" % i)
+        logger.info("Average acceptance probability: %.2f" % np.mean(accepted))
+        logger.info("Average ESS: %.2f" % avg_ess)
+        logger.info("Total time: %.2f" % (time + time_set_up))
