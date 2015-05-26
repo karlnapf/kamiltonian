c8948de259bdbfd04f7e01727ab1e4f6efb3b163
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-05-26 10:21:47 +0100

    initial experiment on abc

---
+++ scripts/experiments/mcmc/abc_target/ground_truth/extract_ground_truth.py
@@ -0,0 +1,71 @@
+from glob import glob
+import pickle
+
+from kmc.tools.Log import logger
+import matplotlib.pyplot as plt
+import numpy as np
+from scripts.experiments.mcmc.independent_job_classes.debug import plot_mcmc_result
+
+
+if __name__ == "__main__":
+    logger.setLevel(10)
+    
+    thin = 200
+    warmup = 1000
+    
+    samples = []
+    
+    fnames = glob('RW_D=10_ground_truth_iterations=3000_*.pkl')
+    for fname in fnames:
+        logger.info("%s" % fname)
+        with open(fname) as f:
+            result = pickle.load(f)
+            # print some summary stats
+            accepted = result.accepted
+            avg_ess = result.posterior_statistics["avg_ess"]
+            min_ess = result.posterior_statistics["min_ess"]
+            
+            time = result.time_taken_sampling
+            time_set_up = result.time_taken_set_up
+        
+            logger.info("Average acceptance probability: %.2f" % np.mean(accepted))
+            logger.info("Average ESS: %.2f" % avg_ess)
+            logger.info("Minimum ESS: %.2f" % min_ess)
+            logger.info("Total time: %.2f" % (time + time_set_up))
+            logger.info("Average ESS/s: %.2f" % (avg_ess / (time + time_set_up)))
+            logger.info("Minimum ESS/s: %.2f" % (min_ess / (time + time_set_up)))
+            
+            if False:
+                plot_mcmc_result(result, D1=1, D2=6)
+        
+        inds = np.arange(warmup, result.num_iterations, step=thin)
+        for i in inds:
+            samples += [result.samples[i]]
+    
+    samples = np.array(samples)
+    if True:
+        with open("benchmark_samples.arr", 'w+') as f:
+            np.save(f, samples)
+    
+    D_pairs = []
+    
+    for D1 in range(9):
+        for D2 in range(D1):
+            D_pairs += [(D1, D2)]
+    
+    for D1, D2 in D_pairs:
+        print(D1, D2)
+        plt.figure(figsize=(8, 12))
+        plt.subplot(521)
+        plt.plot(samples[:, D1])
+        plt.subplot(522)
+        plt.plot(samples[:, D2])
+        plt.subplot(523)
+        plt.hist(samples[:, D1])
+        plt.subplot(524)
+        plt.hist(samples[:, D2])
+        plt.subplot(525)
+        plt.plot(samples[:, D1], samples[:, D2])
+        plt.subplot(526)
+        plt.plot(samples[:, D1], samples[:, D2], '.')
+        plt.show()
---
+++ scripts/experiments/mcmc/abc_target/lite/mcmc_abc_skew_normal_target.py
@@ -0,0 +1,127 @@
+from collections import OrderedDict
+import os
+import time
+
+from independent_jobs.engines.BatchClusterParameters import BatchClusterParameters
+from independent_jobs.engines.SerialComputationEngine import SerialComputationEngine
+from independent_jobs.engines.SlurmComputationEngine import SlurmComputationEngine
+from independent_jobs.tools.FileSystem import FileSystem
+
+from kmc.densities.abc_skew_normal import ABCSkewNormalPosterior
+from kmc.densities.gaussian import IsotropicZeroMeanGaussian
+from kmc.score_matching.lite.gaussian_rkhs_xvalidation import select_sigma_grid,\
+    select_sigma_lambda_cma
+from kmc.tools.Log import logger
+from kmc.tools.convergence_stats import avg_ess, min_ess
+import numpy as np
+from scripts.experiments.mcmc.independent_job_classes.KMCLiteJob import KMCLiteJob
+from scripts.experiments.mcmc.independent_job_classes.MCMCJob import MCMCJobResultAggregatorStoreHome
+from scripts.experiments.mcmc.independent_job_classes.debug import plot_mcmc_result
+
+
+modulename = __file__.split(os.sep)[-1].split('.')[-2]
+statistics = OrderedDict()
+statistics['avg_ess'] = avg_ess
+statistics['min_ess'] = min_ess
+
+def kmc_generator(num_warmup, thin_step):
+    D = 10
+    start = np.random.randn(D) * 0
+    
+    step_size_min = 0.01
+    step_size_max = .1
+    num_steps_min = 50
+    num_steps_max = 50
+    sigma_p = 1.
+    
+    momentum_seed = np.random.randint(time.time())
+    
+    momentum = IsotropicZeroMeanGaussian(sigma=sigma_p, D=D)
+    
+    target = ABCSkewNormalPosterior()
+    Z = np.load("../ground_truth/benchmark_samples.arr")[:1000]
+    learn_parameters = False
+    force_relearn_parameters = False
+    
+    lmbda = 1.
+    sigma = 2 ** 4
+    if False and True:
+        sigma = select_sigma_grid(Z)
+        select_sigma_lambda_cma(Z, num_folds=5, num_repetitions=1, sigma0=0.31, lmbda0=1.)
+        exit()
+        
+    logger.info("Using sigma=%.6f" % sigma)
+    
+    job = KMCLiteJob(Z, sigma, lmbda, target,
+                            momentum, num_iterations,
+                            start,
+                            num_steps_min, num_steps_max, step_size_min, step_size_max,
+                            momentum_seed, learn_parameters, force_relearn_parameters,
+                            statistics, num_warmup, thin_step)
+    
+    # marginal sampler
+    job.recompute_log_pdf = True
+    
+    job.walltime = 60 * 60
+    
+    # store results in home dir straight away
+    d = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1]) + os.sep
+    job.aggregator = MCMCJobResultAggregatorStoreHome(d)
+    
+    return job
+
+if __name__ == "__main__":
+    logger.setLevel(10)
+    num_repetitions = 10
+    
+    # plain MCMC parameters, plan is to use every 200th sample
+    thin_step = 1
+    num_iterations = 5200
+    num_warmup = 200
+    
+    compute_local = False
+    
+    if not FileSystem.cmd_exists("sbatch") or compute_local:
+        engine = SerialComputationEngine()
+        
+    else:
+        johns_slurm_hack = "#SBATCH --partition=intel-ivy,wrkstn,compute"
+        folder = os.sep + os.sep.join(["nfs", "data3", "ucabhst", modulename])
+        batch_parameters = BatchClusterParameters(foldername=folder,
+                                                  resubmit_on_timeout=False,
+                                                  parameter_prefix=johns_slurm_hack)
+        engine = SlurmComputationEngine(batch_parameters, check_interval=1,
+                                        do_clean_up=True)
+        engine.max_jobs_in_queue = 1000
+        engine.store_fire_and_forget = True
+    
+    aggs = []
+    
+    for i in range(num_repetitions):
+        job = kmc_generator(num_warmup, thin_step)
+        logger.info("Repetition %d/%d, %s" % (i + 1, num_repetitions, job.get_parameter_fname_suffix()))
+        aggs += [engine.submit_job(job)]
+        
+    engine.wait_for_all()
+    
+    for i, agg in enumerate(aggs):
+        agg.finalize()
+        result = agg.get_final_result()
+        agg.clean_up()
+
+        if isinstance(engine, SerialComputationEngine):
+            plot_mcmc_result(result, D1=0, D2=1)
+            agg.store_fire_and_forget_result(folder="", job_name="")
+            
+        
+        # print some summary stats
+        accepted = result.accepted
+        avg_ess = result.posterior_statistics["avg_ess"]
+        
+        time = result.time_taken_sampling
+        time_set_up = result.time_taken_set_up
+    
+        logger.info("Repetition %d" % i)
+        logger.info("Average acceptance probability: %.2f" % np.mean(accepted))
+        logger.info("Average ESS: %.2f" % avg_ess)
+        logger.info("Total time: %.2f" % (time + time_set_up))
