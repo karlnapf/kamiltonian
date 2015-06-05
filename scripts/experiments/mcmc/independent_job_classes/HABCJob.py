8aec1a3f116573a2d1fa524ce4eba5424910246f
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-06-05 14:43:10 +0100

    some refactoring and step size changes

--- scripts/experiments/mcmc/independent_job_classes/HABCJob.py
+++ scripts/experiments/mcmc/independent_job_classes/HABCJob.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod
+from numpy.testing.utils import assert_allclose
 import os
 
 from kmc.densities.gaussian import log_gaussian_pdf
+from kmc.tools.Log import logger
 import numpy as np
 from scripts.experiments.mcmc.independent_job_classes.HMCJob import HMCJob, \
     HMCJobResultAggregator
@@ -11,58 +13,90 @@ splitted = __file__.split(os.sep)
 idx = splitted.index('kamiltonian')
 project_path = os.sep.join(splitted[:(idx + 1)])
 
-temp = 0
-
-def SPSA(loss, theta, stepsize):
+def SPSA(loss, theta, stepsize, num_repeats=1):
     """
     Implements Simultaneous perturbation stochastic approximation to estimate
     gradient of given loss function
     
     """
     D = len(theta)
-    delta=2*(np.random.rand(D)>.5).astype(int)-1
-    thetaplus=theta+stepsize*delta;
-    thetaminus=theta-stepsize*delta
-    yplus=loss(thetaplus)
-    yminus=loss(thetaminus);
-    grad_est=(yplus-yminus)/(2*stepsize*delta)
+    grad_ests = np.zeros((num_repeats, D))
     
-    return grad_est
+    for i in range(num_repeats):
+        delta = 2 * (np.random.rand(D) > .5).astype(int) - 1
+        thetaplus = theta + stepsize * delta;
+        thetaminus = theta - stepsize * delta
+        yplus = loss(thetaplus)
+        yminus = loss(thetaminus);
+        grad_ests[i] = (yplus - yminus) / (2 * stepsize * delta)
+    
+    return np.mean(grad_ests, 0)
+
+def test_SPSA():
+    loss = lambda x: 0.5 * x.dot(x)
+    grad_true = lambda x: x
+    
+    x_test = np.array([1, 2])
+    grad_est = SPSA(loss, x_test, stepsize=.05, num_repeats=1000)
+    assert_allclose(grad_true(x_test), grad_est, atol=.1)
+    
+if __name__ == "__main__":
+    test_SPSA()
+    
 
 class DummyHABCTarget(object):
-    def __init__(self, abc_target):
+    def __init__(self, abc_target, num_spsa_repeats=3):
                  
         self.abc_target = abc_target
+        
+        # sticky random numbers: fixed seed to simulate data for this instance of DummyHABCTarget
+        self.fixed_rnd_state = np.random.get_state()
+        self.num_spsa_repeats = num_spsa_repeats
+#         self.current_theta = None
     
     def grad(self, theta):
-#         logger.debug("Computing SPSA gradient")
-        g_est = SPSA(self.log_lik, theta, stepsize=.05)
+        # update likelihood term
+        self._update(theta)
         
-        return g_est + self.abc_target.prior.grad(theta)
+        log_lik = lambda theta: log_gaussian_pdf(theta, self.mu, self.L, is_cholesky=True)
+        
+#         logger.debug("Computing SPSA gradient")
+        grad_lik_est = SPSA(log_lik, theta, stepsize=1., num_repeats=self.num_spsa_repeats)
+        grad_prior = self.abc_target.prior.grad(theta)
+        
+#         logger.debug("grad_lik_est: %s" % str(grad_lik_est))
+#         logger.debug("grad_prior: %s" % str(grad_prior))
+#         logger.debug("||grad_lik_est||: %.2f" % np.linalg.norm(grad_lik_est))
+#         logger.debug("||grad_prior||: %.2f" % np.linalg.norm(grad_prior))
+#         logger.debug("||grad_lik_est-grad_prior||: %.2f" % np.linalg.norm(grad_lik_est-grad_prior))
+        
+        return grad_lik_est + grad_prior
     
-    def update(self, theta):
+    def _update(self, theta):
+#         # dont re-simulate if not needed
+#         if self.current_theta is not None and np.allclose(self.current_theta, theta):
+#             logger.debug("Theta=%s was already simulated from." % str(theta))
+#             return
+#         else:
+#             self.current_theta = theta
+        
         D = self.abc_target.D
         
         # sample pseudo data to fit conditional model
         pseudo_datas = np.zeros((self.abc_target.n_lik_samples, D))
+        
+        # sticky random numbers: fixed seed to simulate data
+        current_state = np.random.get_state()
+        np.random.set_state(self.fixed_rnd_state)
         for i in range(len(pseudo_datas)):
             pseudo_datas[i] = self.abc_target.simulator(theta)
+        np.random.set_state(current_state)
         
         # fit Gaussian, add ridge on diagonal for the epsilon likelihood kernel
         self.mu = np.mean(pseudo_datas, 0)
-        Sigma = np.cov(pseudo_datas) + np.eye(D)*(self.abc_target.epsilon**2)
+        Sigma = np.cov(pseudo_datas) + np.eye(D) * (self.abc_target.epsilon ** 2)
         self.L = np.linalg.cholesky(Sigma)
     
-    def log_lik(self, theta):
-        self.update(theta)
-        return log_gaussian_pdf(theta, self.mu, self.L, is_cholesky=True)
-    
-    def log_pdf(self, theta):
-        self.update(theta)
-        log_lik = self.log_lik(theta)
-            
-        return log_lik  + self.abc_target.prior.log_pdf(theta)
-
 class HABCJob(HMCJob):
     def __init__(self, abc_target, momentum,
                  num_iterations, start,
