e9d1296f854fa0c21f7868778759ab3a1dd70b97
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-08-05 14:25:48 +0100

    mini update

--- scripts/experiments/trajectories/random_feats/plot_csv_results.py
+++ scripts/experiments/trajectories/random_feats/plot_csv_results.py
@@ -12,6 +12,8 @@ modulename = __file__.split(os.sep)[-1].split('.')[-2]
 
 if __name__ == "__main__":
     fname = "gaussian_target/results.csv"
+    fname = "rebuttal_target/average_accept_rebuttal_local.csv"
+    
 #     fname = "laplace_target/results.csv"
 #     fname = "student_target/average_accept_student_local.csv"
     
@@ -19,15 +21,15 @@ if __name__ == "__main__":
     
     Ds, Ns, _ = gen_sparse_2d_array_from_dict(result_dict_from_file(fname), lambda x: None)
 
-    plot_repetitions_heatmap(fname)
-    plot_trajectory_result_heatmap(fname)
-    plt.show()
+#     plot_repetitions_heatmap(fname)
+#     plot_trajectory_result_heatmap(fname)
+#     plt.show()
     
     for D in Ds:
         plot_trajectory_result_mean_fixed_D(fname, D=D)
  
-    for N in Ns:
-        plot_trajectory_result_mean_fixed_N(fname, N=N)
-     
-    plot_trajectory_result_necessary_data(fname, [0.1, 0.3, 0.5, 0.7])
+#     for N in Ns:
+#         plot_trajectory_result_mean_fixed_N(fname, N=N)
+#      
+#     plot_trajectory_result_necessary_data(fname, [0.1, 0.3, 0.5, 0.7])
     plt.show()
