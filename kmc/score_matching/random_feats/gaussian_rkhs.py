b6375081c7b5d22b44941fe291208e67d3d6e57a
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-08-05 14:24:30 +0100

    added rational quadratic kernel

--- kmc/score_matching/random_feats/gaussian_rkhs.py
+++ kmc/score_matching/random_feats/gaussian_rkhs.py
@@ -10,6 +10,53 @@ def sample_basis(D, m, gamma):
     
     return omega, u
 
+def sample_basis_rational_quadratic(D, m, alpha, beta=1., return_taus=False):
+    """
+    Given a Gaussian kernel of the form
+    k(x,y) = \exp(-\gamma  ||x-y||^2)
+           = \exp(-0.5  \tau   ||x-y||^2),
+           
+    where
+    \gamma = 0.5  \tau,
+    
+    this method returns a random Fourier features basis for an infinite mixture
+    of Gaussian kernels (aka rational quadratic kernel)
+    k(x,y) = \int d\gamma p(\tau) k(x,y),
+    where p(\tau) is a Gamma distribution
+    \tau \sim \texttt{Gamma}(\tau | \alpha,\beta), parametrised with
+    \alpha - shape parameter
+    \beta -  mean parameter (mean=shape*scale = shape/rate)
+    
+    The parametrisation is such that alpha, beta correspond to the closed form RQ kernel
+    k(x,y) = (1+ (||x-y||^2 \tau) / (2 \alpha)),
+           = (1+ (||x-y||^2) / (2 \alpha \sigma^2)),
+    where \tau = \sigma^2, which is the standard form given in textbooks.
+    
+    I.e. in the GPML book, Chapter 4.
+    """
+    
+    omega = np.zeros((D, m))
+    taus = np.zeros(m)
+    
+    # sample from mixture of Gaussians
+    # where the length scales are distributed according to a Gamma
+    for i in range(m):
+        # each sample has a different length scale
+        #     
+        #     mean = shape/rate = shape * scale
+        # <=> scale = mean/shape = beta/alpha
+        tau = np.random.gamma(shape=alpha, scale=beta / alpha)
+        taus[i] = tau
+        gamma = 0.5 * tau
+        omega[:, i] = gamma * np.random.randn(D)
+    
+    u = np.random.uniform(0, 2 * np.pi, m)
+    
+    if return_taus:
+        return omega, u, taus
+    else:
+        return omega, u
+
 def feature_map_single(x, omega, u):
     m = 1 if np.isscalar(u) else len(u)
     return np.cos(np.dot(x, omega) + u) * np.sqrt(2. / m)
