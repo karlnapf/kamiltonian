43ec952abbbd7c8c6f52ae5543cf95ce0e71b3f5
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-10-29 16:19:30 +0000

    added current set of results for rebuttal

--- scripts/experiments/trajectories/random_feats/plot_csv_results.py
+++ scripts/experiments/trajectories/random_feats/plot_csv_results.py
@@ -13,6 +13,8 @@ modulename = __file__.split(os.sep)[-1].split('.')[-2]
 if __name__ == "__main__":
     fname = "gaussian_target/results.csv"
     fname = "rebuttal_target/average_accept_rebuttal_local.csv"
+    fname = "rebuttal_target/average_accept_rebuttal.csv"
+    
     
 #     fname = "laplace_target/results.csv"
 #     fname = "student_target/average_accept_student_local.csv"
@@ -21,15 +23,19 @@ if __name__ == "__main__":
     
     Ds, Ns, _ = gen_sparse_2d_array_from_dict(result_dict_from_file(fname), lambda x: None)
 
-#     plot_repetitions_heatmap(fname)
-#     plot_trajectory_result_heatmap(fname)
-#     plt.show()
+    plot_repetitions_heatmap(fname)
+    plot_trajectory_result_heatmap(fname)
+    plt.savefig('average_accept_gaussian_target_rebuttal_kmc.eps', bboxinches='tight')
     
-    for D in Ds:
-        plot_trajectory_result_mean_fixed_D(fname, D=D)
- 
-#     for N in Ns:
-#         plot_trajectory_result_mean_fixed_N(fname, N=N)
-#      
-#     plot_trajectory_result_necessary_data(fname, [0.1, 0.3, 0.5, 0.7])
+    
+    D = 8
+    plot_trajectory_result_mean_fixed_D(fname, D=D)
+    plt.savefig('average_accept_gaussian_target_rebuttal_D=%d.eps' % D, bboxinches='tight')
+    
+    N=5000
+    plot_trajectory_result_mean_fixed_N(fname, N=N)
+    plt.savefig('average_accept_gaussian_target_rebuttal_N=%d.eps' % N, bboxinches='tight')
+    
+      
+    plot_trajectory_result_necessary_data(fname, [0.05, 0.2, 0.3, 0.5])
     plt.show()
